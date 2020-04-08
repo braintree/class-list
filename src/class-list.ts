209eaaa091a96d0c8ac6cc6d700ece00b4e54185
Blade Barringer <blade.barringer@getbraintree.com>
2020-04-08 18:03:46 +0000

    Switch to arrow functions

--- src/class-list.ts
+++ src/class-list.ts
@@ -4,23 +4,19 @@ function _classesOf(element: HTMLElement): string[] {
 
 export function add(element: HTMLElement, ...args: string[]): void {
   const toAdd = Array.from(args);
-  const className = _classesOf(element)
+
+  element.className = _classesOf(element)
     .filter((classname) => toAdd.indexOf(classname) === -1)
     .concat(toAdd)
     .join(" ");
-
-  element.className = className;
 }
 
 export function remove(element: HTMLElement, ...args: string[]): void {
   const toRemove = Array.from(args);
-  const className = _classesOf(element)
-    .filter(function (classname) {
-      return toRemove.indexOf(classname) === -1;
-    })
-    .join(" ");
 
-  element.className = className;
+  element.className = _classesOf(element)
+    .filter((classname) => toRemove.indexOf(classname) === -1)
+    .join(" ");
 }
 
 export function toggle(
