73d7732f6973db750e2ff64443b6b2881867e50b
Blade Barringer <blade.barringer@getbraintree.com>
2020-07-15 16:57:10 +0000

    Fixup class-list for publishing

--- src/class-list.ts
+++ src/class-list.ts
@@ -2,18 +2,14 @@ function _classesOf(element: HTMLElement): string[] {
   return element.className.trim().split(/\s+/);
 }
 
-export function add(element: HTMLElement, ...args: string[]): void {
-  const toAdd = Array.from(args);
-
+export function add(element: HTMLElement, ...toAdd: string[]): void {
   element.className = _classesOf(element)
     .filter((classname) => toAdd.indexOf(classname) === -1)
     .concat(toAdd)
     .join(" ");
 }
 
-export function remove(element: HTMLElement, ...args: string[]): void {
-  const toRemove = Array.from(args);
-
+export function remove(element: HTMLElement, ...toRemove: string[]): void {
   element.className = _classesOf(element)
     .filter((classname) => toRemove.indexOf(classname) === -1)
     .join(" ");
